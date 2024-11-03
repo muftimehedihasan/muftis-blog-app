6aac6193407ea2e89edf62d961a83dbd19c81873
Mufti Mehedi Hasan <130230722+muftimehedihasan@users.noreply.github.com>
2024-11-03 21:04:20 +0600

    new commit

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\FrontCategoryController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SinglePostController;
 
@@ -22,6 +23,9 @@ Route::middleware('auth')->prefix('admin/')
 ->name('admin.')
 ->group(function () {
     Route::resource('/categories', CategoryController::class);
+    Route::resource('/posts', PostController::class);
+
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
